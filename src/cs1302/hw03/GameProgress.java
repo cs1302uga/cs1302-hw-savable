fa71f692b6efaf13e45cda7f99e066ed2b17c7fe
Brad Barnes <bjb211@uga.edu>
2023-01-29 13:10:16 -0500

    Initial version of starter code

---
+++ src/cs1302/hw03/GameProgress.java
@@ -0,0 +1,71 @@
+package cs1302.hw03;
+
+/**
+ * Represents the progress made in a video game. Each object stores the user's current
+ * score and level.
+ */
+public class GameProgress {
+    /** Represents the user's current score in the game.*/
+    private int score;
+
+    /** Represents the level the user is on in the game.*/
+    private int level;
+
+    /**
+     * Creates a new {@code GameProgress} object with the specified score and level
+     * values.
+     *
+     * @param score the user's current score in the game. This can be negative.
+     * @param level the user's current level in the game.
+     * @throws IllegalArgumentException when level is not positive.
+     */
+    public GameProgress(int score, int level) {
+        setScore(score);
+        setLevel(level);
+    } // GameProgress
+
+    /**
+     * Returns the user's current score in the game.
+     *
+     * @return the user's current score.
+     */
+    public int getScore() {
+        return this.score;
+    } // getScore
+
+    /**
+     * Returns the user's current level in the game.
+     *
+     * @return the user's current level.
+     */
+    public int getLevel() {
+        return this.level;
+    } // getLevel
+
+    /**
+     * Sets the user's current score to the specified {@code score} value.
+     * Negative score values are allowed.
+     *
+     * @param score the updated score value.
+     */
+    public void setScore(int score) {
+        this.score = score;
+    } // setScore
+
+    /**
+     * Sets the user's current level to the specified {@code level} value.
+     * Negative level values are not allowed.
+     *
+     * @param level the updated level value.
+     * @throws IllegalArgumentException if {@code level} is not positive.
+     */
+    public void setLevel(int level) {
+        if (level > 0) {
+            this.level = level;
+        } else {
+            IllegalArgumentException iae = new IllegalArgumentException("Level must be positive");
+            throw iae;
+        } // if
+    } // set Level
+
+} // GameProgress
---
+++ src/cs1302/hw03/TextBook.java
@@ -0,0 +1,59 @@
+package cs1302.hw03;
+
+/**
+ * Represents a textbook used for a class. Each object stores the book's title
+ * and publisher.
+ */
+public class TextBook {
+    private String title;
+    private String publisher;
+
+    /**
+     * Creates a new {@code TextBook} object with the specified title and publisher
+     * values.
+     *
+     * @param title the book's title.
+     * @param publisher the book's publisher.
+     */
+    public TextBook(String title, String publisher) {
+        setTitle(title);
+        setPublisher(publisher);
+    } // TextBook
+
+    /**
+     * Returns the book's title.
+     *
+     * @return the book's title.
+     */
+    public String getTitle() {
+        return this.title;
+    } // getTitle
+
+    /**
+     * Returns the book's publisher.
+     *
+     * @return the book's publisher.
+     */
+    public String getPublisher() {
+        return this.publisher;
+    } // getPublisher
+
+    /**
+     * Sets the book's title to the specified {@code title} value.
+     *
+     * @param title the updated book title.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    } // setTitle
+
+    /**
+     * Sets the book's publisher to the specified {@code publisher} value.
+     *
+     * @param publisher the updated book publisher.
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    } // set Publisher
+
+} // TextBook
